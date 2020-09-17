3ff8baefff976d83948bda02cf906eacf4b47209
jiawenquan <1173551915@qq.com>
2020-09-17 23:28:18 +0800

    增加规则，修改权重类型，增加增强抑制函数

--- BaseNeuron.cs
+++ BaseNeuron.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using MathNet.Numerics.LinearAlgebra;
-using MathNet.Numerics.LinearAlgebra.Double;
+//using MathNet.Numerics.LinearAlgebra;
+//using MathNet.Numerics.LinearAlgebra.Double;
 namespace DistributedNeuron
 {
 
     /// <summary>
     ///  脑主体: 
     ///  外部主输入神经网络,(分布式传感器网络) 外部神经细胞的连接的权重更稳定
+    ///  
+    ///  初始神经元相互的连接会很对、等过发展一定阶段、神经元之间的流通逐渐稳定、流通权重很低的神经元逐渐断开,
+    ///  流通权重一值保持很高的神经元逐渐找到神经元的最短连通路径,建立新的连接
+    ///  神经源 之前的连接 w 值保证一定的随机性,
+    ///  构建类 内分泌神经的激素(多巴胺、内啡肽)奖励与衰减系统, 对于整体神经元 干涉调控
     /// </summary>
 
     class BaseNeuron
@@ -21,8 +26,8 @@ namespace DistributedNeuron
 
 
         // 主连接、脉络连接、网连接( 神经元集合单元 从小单元到大单元、逐级增加)
-        Dictionary<string, double> inputConnectedNeurons = new Dictionary<string, double>();  // 输入神经元的 id 与权重     （随机初始值大部分接近平均值 0）  
-        Dictionary<string, double> outputConnectedNeurons = new Dictionary<string, double>();  // 输入神经元的 id 与权重    （随机初始值大部分接近平均值 0） 
+        Dictionary<string, float> inputConnectedNeurons = new Dictionary<string, float>();  // 输入神经元的 id 与权重     （随机初始值大部分接近平均值 0）  
+        Dictionary<string, float> outputConnectedNeurons = new Dictionary<string, float>();  // 输入神经元的 id 与权重    （随机初始值大部分接近平均值 0） 
 
         public BaseNeuron()
         {
@@ -51,8 +56,18 @@ namespace DistributedNeuron
         {
             //  当输出的权重值大于现有的权重值,对此输出的权重值激活正负增强（-1，1 区间内） 
         }
-        // 输入神经元
+        
+        //固定帧更新
         public void Update()
+        {
+            //对于没有长时间激活的 神经源连接权重 随着时间与频率的推移回归平均值
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="weight">类似激素调控, 对神经元的 整体 w 进行激励放大，与惩罚减小</param>
+        public void  UpdateWeight(float weight)
         {
             //对于没有长时间激活的 神经源连接权重 随着时间与频率的推移回归平均值
         }
